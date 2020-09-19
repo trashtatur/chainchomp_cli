3b4cf52b1969c0aa7b640b1f2f31d262cbd18b1f
Andreas Hörster <andreas-hoerster@web.de>
2020-09-19 14:37:49 +0200

    Minor adjustments in documentation of edit command

--- chainchomp_cli/src/core/commands/chainlink_edit/chainlink_edit_command.py
+++ chainchomp_cli/src/core/commands/chainlink_edit/chainlink_edit_command.py
@@ -10,12 +10,12 @@ def chainlink_edit(path):
     parameters:
     path: Absolute path to the config file. Defaults to current working directory
 
-    This command allows you to edit a chainlink file at a given location.
+    This command allows you to edit a Chainfile at a given location.
     Chainchomp will look for a file called "chainfile.yml" here.
-    If you provide the file directy, through a path, it will also work.
+    If you provide the file directly, through a path, it will also work.
     If it can't be found the command won't do anything.
 
-    If the file is found chainchomp will allow you to edit it through command prompts
+    If the file is found Chainchomp will allow you to edit it through command prompts
     :return:
     """
     pass
