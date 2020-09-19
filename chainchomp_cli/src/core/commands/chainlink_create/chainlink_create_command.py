68f4fc73543eb26b8e16389069f59fccea475d6c
Andreas Hörster <andreas-hoerster@web.de>
2020-09-19 14:37:00 +0200

    Added project choice to chainlink create command

--- chainchomp_cli/src/core/commands/chainlink_create/chainlink_create_command.py
+++ chainchomp_cli/src/core/commands/chainlink_create/chainlink_create_command.py
@@ -1,8 +1,10 @@
 import os
-
 import click
 from chainchomplib.configlayer.model.ChainlinkConfigModel import ChainlinkConfigModel
 from click import echo, style
+from chainchomp_cli.src.core.handlers.projects.ProjectFileHandler import ProjectFileHandler
+from chainchomp_cli.src.core.handlers.projects.ProjectsFolderHandler import ProjectsFolderHandler
+from chainchomp_cli.src.core.handlers.setup.SetupHandler import SetupHandler
 
 
 @click.command('chainlink:create')
@@ -24,17 +26,38 @@ def chainlink_create(path):
     :return:
     """
 
+    if not SetupHandler.is_setup():
+        echo(style('Oh no it looks like you haven\'t set up chainchomp on this machine yet. Please run '
+                   'chainchomp:setup before you continue', fg='red'))
+        return
+
     echo(style('Welcome to Chainchomp. We can see that you want to add a new chainlink', fg='cyan'))
     echo(style('What is the project that this chainlink is assigned to', fg='cyan'))
-    # TODO list currently available projects and offer choice before new project needs to be created
-    project_name = click.prompt('Please type in the designated project name now')
+    projects_list = ProjectsFolderHandler.provide_list_of_projects()
+    user_wants_new_project = False
+    if projects_list is not []:
+        output_string = f'Press 0 to make a new project \n'
+        counter = 1
+        for project in projects_list:
+            output_string += f'{counter}) {project} \n'
+        echo(style('You can choose from already existing projects, or make a new one', fg='cyan'))
+        project_name_index = click.prompt(output_string, default=0)
+        if project_name_index == 0:
+            user_wants_new_project = True
+        if project_name_index != 0:
+            project_name = projects_list[project_name_index - 1]
+    if user_wants_new_project:
+        project_name = click.prompt('Please type in the designated project name now')
+
     echo(style('Now please provide a name for your chainlink', fg='cyan'))
     chainlink_name = click.prompt('Please type in the designated name now')
     chainlink_config_model = ChainlinkConfigModel(project_name, chainlink_name)
 
     more_information = click.confirm(style('Do you want to input further details?', fg='cyan'), default=False)
     if not more_information:
+        create_or_add_to_project(project_name, chainlink_name, user_wants_new_project)
         return
+
     master_link = click.confirm('Is this chainlink a Master link?', default=chainlink_config_model.is_master_link)
     if not master_link:
         master_location = click.prompt('Do you want to provide information about the master link location?',
@@ -67,3 +90,13 @@ def chainlink_create(path):
                                '2) Apache Kafka\n',
                                default=default)
         chainlink_config_model.mq_type = mq_type
+
+    create_or_add_to_project(project_name, chainlink_name, user_wants_new_project)
+
+
+def create_or_add_to_project(project_name: str, chainlink_name: str, new: bool):
+    project_file_handler = ProjectFileHandler()
+    if new:
+        project_file_handler.create_new_project(project_name, [chainlink_name])
+    else:
+        project_file_handler.add_chainlink_to_project(chainlink_name, project_name)
